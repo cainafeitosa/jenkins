790a62a5634bb3d1b4e28e9e99319669563aa179
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-09 17:41:16 -0300

    Testar

--- pipeline/libraries/maven/steps/unit_tests.groovy
+++ pipeline/libraries/maven/steps/unit_tests.groovy
@@ -1,5 +1,15 @@
 void call() {
   stage("Maven: Test") {
-    println "mvn -B test"
+    podTemplate(
+      containers: [
+        containerTemplate(name: 'maven', image: 'maven:3-jdk-11', command: 'sleep', args: '9999999', ttyEnabled: true)
+      ]
+    ) {
+      node(POD_LABEL) {
+        container('maven') {
+          sh 'mvn --version'
+        }
+      }
+    }
   }
 }
