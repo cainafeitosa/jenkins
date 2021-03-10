09918506330045270a3ac312f5b5303aed554778
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-10 17:13:15 -0300

    Testar

--- pipeline/libraries/agent/steps/jenkins_runner.groovy
+++ pipeline/libraries/agent/steps/jenkins_runner.groovy
@@ -3,7 +3,16 @@
 void call(Closure body) {
     if (config.kubernetes) {
         def cloud = config.kubernetes.cloud ?: 'kubernetes'
-        podTemplate(cloud: cloud) {
+        podTemplate(cloud: cloud, name: 'maven', containers: [
+            containerTemplate(
+                name: 'maven',
+                image: 'maven:3-jdk-8',
+                command: 'sleep',
+                args: '999999',
+                ttyEnabled: true
+            )
+        ])
+        podTemplate(cloud: cloud, inheritFrom: 'maven') {
             node(POD_LABEL) {
                 body()
             }
