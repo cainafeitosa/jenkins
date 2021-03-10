9516bf84282362efe6404f5d06bbd7d41aa35cfb
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-10 14:22:16 -0300

    Testar

--- pipeline/configuration/pipeline_config.groovy
+++ pipeline/configuration/pipeline_config.groovy
@@ -1,7 +1,15 @@
+libraries {
+  runner
+  maven
+  docker
+  sonarqube
+}
+
 stages {
   continuous_integration {
-    compile
     unit_tests
-    build
+    package_artifact
+    static_code_analysis
+    build_container_image
   }
 }
