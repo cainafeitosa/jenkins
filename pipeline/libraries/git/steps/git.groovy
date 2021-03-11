b9dc442f796dc586d066296161c7c115ebfe040d
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-11 16:00:54 -0300

    Testar

--- pipeline/libraries/git/steps/git.groovy
+++ pipeline/libraries/git/steps/git.groovy
@@ -1,5 +1,24 @@
 #!/usr/bin/env groovy
 
 void checkout() {
-    println 'checkout'
+    defaults = [
+        excludedMessage: '',
+        excludedRegions: '',
+        includedRegions: '',
+    ]
+
+    def excludedMessage = config.excludedMessage ?: defaults.excludedMessage
+    def excludedRegions = config.excludedRegions ?: defaults.excludedRegions
+    def includedRegions = config.includedRegions ?: defaults.includedRegions
+
+    checkout([
+        $class: 'GitSCM',
+        branches: scm.branches,
+        extensions: [
+            [$class: 'MessageExclusion', excludedMessage: excludedMessage],
+            [$class: 'LocalBranch'],
+            [$class: 'PathRestriction', excludedRegions: excludedRegions, includedRegions: includedRegions]
+        ],
+        userRemoteConfigs: scm.userRemoteConfigs
+    ])
 }
