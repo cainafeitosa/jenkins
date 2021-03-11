8d78a5236df5b76fa254d88aed569b85a5429f20
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-11 00:20:32 -0300

    Testar

--- pipeline/libraries/maven/steps/build.groovy
+++ pipeline/libraries/maven/steps/build.groovy
@@ -4,6 +4,8 @@ void call(Closure body) {
     stage('Maven: Compile') {
         if (pipelineConfig.libraries.agent.kubernetes) {
             container('maven') {
+                sh 'pwd'
+                sh 'ls -la'
                 sh 'mvn -B clean compile'
             }
         }
