a1a377a80671ecbd9754efa05d0a3c51a20e4bd3
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-25 15:37:29 -0300

    Testar

--- pipeline/libraries/maven/library_config.groovy
+++ pipeline/libraries/maven/library_config.groovy
@@ -2,7 +2,12 @@
 
 fields {
   	optional {
-		runs_on = Map
+		runs_on {
+			kubernetes {
+				cloud = String
+				pod_template = String
+			}
+		}
 		cli_options = String
 		test_results_path = String
   	}
