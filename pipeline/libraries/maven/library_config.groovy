b769ef808e65be186db0cd54477fcda43422e81b
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-25 15:35:35 -0300

    Testar

--- pipeline/libraries/maven/library_config.groovy
+++ pipeline/libraries/maven/library_config.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 
-// fields {
-//   	optional {
-// 		cli_options = String
-// 		test_results_path = String
-//   	}
-// }
+fields {
+  	optional {
+		runs_on = Map
+		cli_options = String
+		test_results_path = String
+  	}
+}
