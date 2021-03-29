1626d9365c609873a511f9c067d00f7955fbd584
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-29 15:35:25 -0300

    Testar

--- pipeline/configuration/pipeline_config.groovy
+++ pipeline/configuration/pipeline_config.groovy
@@ -39,10 +39,10 @@ application_environments {
     dev {
         long_name = "Development"
     }
-    test {
+    tst {
         long_name = "Test"
     } 
-    prod {
+    prd {
         long_name = "Production"
     }
 }
