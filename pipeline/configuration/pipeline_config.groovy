22fa40710bca328a7b940c16890388ec54507457
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-29 15:08:56 -0300

    Testar

--- pipeline/configuration/pipeline_config.groovy
+++ pipeline/configuration/pipeline_config.groovy
@@ -1,51 +1,51 @@
 @merge libraries {
-	agent
-	utility
-	git
+    agent
+    utility
+    git
     @merge docker {
-		pod_template = "docker"
+        pod_template = "docker"
         registry = "http://registry.apps.lab.local"
         credentials_id = "registry-credential"
     }
 }
 
 application_environments {
-	dev {
-		long_name = "Development"
-	}
-	test {
-		long_name = "Test"
-	} 
-	prod {
-		long_name = "Production"
-	}
+    dev {
+        long_name = "Development"
+    }
+    test {
+        long_name = "Test"
+    } 
+    prod {
+        long_name = "Production"
+    }
 }
 
 keywords {
-	master  =  /^[Mm]aster$/
-	develop =  /^[Dd]evelop(ment|er|)$/
-	hotfix  =  /^[Hh]ot[Ff]ix-/
-	release =  /^[Rr]elease-(\d+.)*\d$/
+    master  =  /^[Mm]aster$/
+    develop =  /^[Dd]evelop(ment|er|)$/
+    hotfix  =  /^[Hh]ot[Ff]ix-/
+    release =  /^[Rr]elease-(\d+.)*\d$/
 }
 
 template_methods {
-	package
-	build_image
-	unit_test
-	static_code_analysis
-	publish_image
+    package
+    build_image
+    unit_test
+    static_code_analysis
+    publish_image
 }
 
 stages {
-	build {
-		package
-		build_image
-	}
-	test {
-		unit_test
-		static_code_analysis
-	}
-	release {
-		publish_image
-	}
+    build {
+        package
+        build_image
+    }
+    test {
+        unit_test
+        static_code_analysis
+    }
+    release {
+        publish_image
+    }
 }
