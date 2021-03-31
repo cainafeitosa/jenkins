332a06fd2e73adc328dc1cb6637f07c73331dfda
Cainã Feitosa Araújo <cainafeitosa@live.com>
2021-03-31 19:32:42 -0300

    Corrigir biblioteca agent

--- pipeline/libraries/docker/steps/docker.groovy
+++ pipeline/libraries/docker/steps/docker.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 void call(String args) {
-    def commandLine = "docker ${args}"
+    def commandLine = 'docker ' + args
 
     if (config.pod_template) {
         container("docker") {
